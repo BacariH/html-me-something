ceff2759485b309ed01ea11d506b12e967cd7ecc
Grrrdolf <herseyb71@gmail.com>
2020-12-03 14:03:05 -0600

    Several changes have been made to the content

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
 var slideIndex = 0;
 showSlides();
 
+function showLocation(n){
+    showSlides(slideIndex = n);
+    
+}
+
 function showSlides(){
     let i;
     let places = document.querySelectorAll('.places');
-    console.log(places)
+    let cityNames = document.querySelectorAll('a')
+    
+
     for(i = 0; i < places.length; i++){
         places[i].style.display = "none";
+        cityNames[i].className = ' ';
     }
 
     slideIndex++;
 
     if(slideIndex > places.length){slideIndex = 1};
     places[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000);
+    cityNames[slideIndex-1].className = "active";
+
+    setTimeout(showSlides, 5000);
 }
+
+
