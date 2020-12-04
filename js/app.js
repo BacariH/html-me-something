439a89a4f97a7aa62f0fba30196eed1fc9862b2b
Grrrdolf <herseyb71@gmail.com>
2020-12-03 18:37:38 -0600

    Done for today

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 var slideIndex = 0;
+var slideTimer;
 showSlides();
 
-function showLocation(n){
-    showSlides(slideIndex = n);
-    
-}
 
 function showSlides(){
     let i;
     let places = document.querySelectorAll('.places');
     let cityNames = document.querySelectorAll('a')
-    
+    slideTimer = setTimeout(showSlides, 5000);
 
     for(i = 0; i < places.length; i++){
         places[i].style.display = "none";
@@ -23,7 +20,10 @@ function showSlides(){
     places[slideIndex-1].style.display = "block";
     cityNames[slideIndex-1].className = "active";
 
-    setTimeout(showSlides, 5000);
+    slideTimer;
 }
 
-
+function showLocation(n){
+    showSlides(slideIndex = n);
+    clearInterval(slideTimer);
+}
